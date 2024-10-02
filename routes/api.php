e520b7f7fc92b5166829b73de44669ad38d5138c
Fredy Palacios <fredy_palacios_online@hotmail.com>
2024-10-02 13:11:31 +0200

    Unnecessary comments deleted
    Update .DS_Store
    
    Update README.md

--- routes/api.php
+++ routes/api.php
@@ -19,26 +19,19 @@ Route::post('/login',[UserAuthController::class,'login']);
 //User routes
 Route::middleware(['auth:api', 'role:user'])->prefix('user')->group(function () {
     Route::post('/ticket/create',[TicketController::class,'store']);
-    //Close ticket
     Route::post('/ticket/{ticket}/close',[TicketController::class,'updateStatusToClosed']);
-    //Get all tickets by user
     Route::get('/tickets',[UserController::class,'getAllTicketsByUser']);
-    //Create message
     Route::post('/ticket/{ticket}/message',[MessageController::class,'store']);
-    //Get all messages
     Route::get('/ticket/{ticket}/messages',[MessageController::class,'getMessages']);
 
 });
 
 //Agent routes
 Route::middleware(['auth:api', 'role:agent'])->prefix('agent')->group(function () {
-    //routes to change status
     Route::post('/ticket/pending/{ticket}',[TicketController::class,'updateStatusToPending']);
     Route::post('/ticket/resolved/{ticket}',[TicketController::class,'updateStatusToResolved']);
     Route::post('/ticket/closed/{ticket}',[TicketController::class,'updateStatusToClosed']);
-    //create message
     Route::post('/ticket/{ticket}/message',[MessageController::class,'store']);
-    //get all messages
     Route::get('/ticket/{ticket}/messages',[MessageController::class,'getMessages']);
 });
 
